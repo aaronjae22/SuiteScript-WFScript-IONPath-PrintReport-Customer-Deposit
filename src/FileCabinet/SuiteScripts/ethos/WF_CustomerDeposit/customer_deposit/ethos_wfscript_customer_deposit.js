ba350c1f93cd1c5b1867f2c50fef85a7e1e94fa5
Aaron Ayerdis Espinoza <aaronaej7@gmail.com>
2023-03-07 09:06:07 -0600

    2. Getting Primary Info and Event Info from Netsuite

--- src/FileCabinet/SuiteScripts/ethos/WF_CustomerDeposit/customer_deposit/ethos_wfscript_customer_deposit.js
+++ src/FileCabinet/SuiteScripts/ethos/WF_CustomerDeposit/customer_deposit/ethos_wfscript_customer_deposit.js
@@ -16,18 +16,80 @@ define(['N/file', 'N/query', 'N/record', 'N/render'],
          */
         const onAction = (scriptContext) => {
 
+            const customerDeposit = scriptContext.newRecord;
+
             log.debug({ title : 'Testing Workflow Action Script', details : 'WFScript' });
 
-            generateReport();
+            // Getting Payment Events List Info from Netsuite //
+            let paymentEventsListName = 'paymentevent';
+            let lineCount = customerDeposit.getLineCount({sublistId : paymentEventsListName});
+
+            // Getting Customer Deposit Primary Info //
+            let customer = customerDeposit.getValue({fieldId : 'entityname'});
+            let salesOrder = customerDeposit.getValue({fieldId : 'salesorder'});
+            let currency = customerDeposit.getValue({fieldId : 'currencyname'});
+            let postingPeriod = customerDeposit.getValue({fieldId : 'postingperiod'});
+            let account = customerDeposit.getValue({fieldId : 'account'});
+            let deposit = customerDeposit.getValue({fieldId : 'tranid'});
+            let paymentAmount = customerDeposit.getValue({fieldId : 'payment'});
+
+            let depositData = {
+                customer,
+                salesOrder,
+                currency,
+                postingPeriod,
+                account,
+                deposit,
+                paymentAmount,
+                paymentEvents : [],
+            };
+
+            // Getting Payment Events Info //
+            for (let i = 0; i < lineCount; i++)
+            {
+                let eventRecord =
+                    {
+                        transaction : customerDeposit.getSublistValue({
+                            sublistId : paymentEventsListName,
+                            fieldId : 'owningtransaction',
+                            line : i}),
+                        tranEvent : customerDeposit.getSublistValue({
+                            sublistId : paymentEventsListName,
+                            fieldId : 'type',
+                            line : i}),
+                        payment : customerDeposit.getSublistValue({
+                            sublistId : paymentEventsListName,
+                            fieldId : 'card',
+                            line : i}),
+                        result : customerDeposit.getSublistValue({
+                            sublistId : paymentEventsListName,
+                            fieldId : 'result',
+                            line : i}),
+                        amount : customerDeposit.getSublistValue({
+                            sublistId : paymentEventsListName,
+                            fieldId : 'amount',
+                            line : i}),
+                        date : customerDeposit.getSublistValue({
+                            sublistId : paymentEventsListName,
+                            fieldId : 'eventdate',
+                            line : i}),
+                    };
+
+                depositData.paymentEvents.push(eventRecord);
+                log.debug({ title : 'Iterating over Event: ' + 1 , details : depositData });
+            }
+
+
+            // generateReport();
 
         }
 
-        const generateReport = () =>
+        /* const generateReport = () =>
         {
             let renderer = render.create();
 
             log.debug({ title : 'Testing Renderer', details : renderer});
-        }
+        } */
 
         return {onAction};
     });
